b2abdc6e62c80a7abde2ca8bbcd67ee7d7d7cca6
Krystian <krystian.kuzminski@programa.pl>
2024-01-18 17:18:34 +0100

    Add nav component

---
+++ src/componenets/Nav.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="#">Samochody</a></li>
+        <li><a href="#">Jazda próbna</a></li>
+        <li><a href="#">Oferta</a></li>
+        <li><a href="#">Salony Cupra</a></li>
+    </ul>
+</nav>
